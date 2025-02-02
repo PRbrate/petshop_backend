157ac77b4908c68526474adafd1492754f97ffcc
Paulo Roberto <86695830+PRbrate@users.noreply.github.com>
2025-02-02 17:05:55 -0300

    BEE-05 FOR APROVATION

---
+++ PetShop.Domain/Entities/JwtSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace PetShop.Domain.Entities
+{
+    public static class JwtSettings
+    {
+        public static string Secret { get; set; } = "AuthomatizeCompanyMaster@GroupProgramer#!2025Yers,Gutavo<Pasqua>Tech.Employer8";
+        public static int HourExpire { get; set; } = 1;
+    }
+}
